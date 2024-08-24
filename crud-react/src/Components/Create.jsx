9a73cbb2487e4cdd454c649ad15f51aa79e29d1c
Anshuman Singh <anshumansingh005@gmail.com>
2024-08-25 03:12:36 +0530

    provided states and made form interactive.

--- crud-react/src/Components/Create.jsx
+++ crud-react/src/Components/Create.jsx
@@ -1,23 +1,49 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import axios from "axios";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 const Create = () => {
+  const [name, setName] = useState("create");
+  const [email, setEmail] = useState("");
+  const header = { "Access-Conterol-Allow-Origin": "*" };
+  const history = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Clicked");
+    axios.post("https://66c741f8732bf1b79fa5eff3.mockapi.io/crud-youtube", {
+      name: name,
+      email: email,
+      header,
+    });
+    history("/allBooking");
+  };
   return (
-    <div>
-      <Form>
+    <>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="forBasicName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" />
+          <Form.Control
+            type="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
         </Button>
       </Form>
-    </div>
+    </>
   );
 };
 
