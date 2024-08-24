a7dc2c52f367506d3d6f566c002cb06130e8f19b
Anshuman Singh <anshumansingh005@gmail.com>
2024-08-25 03:11:39 +0530

    added root router and defined 2 roots

--- crud-react/src/main.jsx
+++ crud-react/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { StrictMode } from "react";
+import { Container } from "react-bootstrap";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Read from "./Components/Read.jsx";
+import "./index.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/allBooking",
+    element: <Read />,
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Container>
+      <RouterProvider router={router} />
+    </Container>
+  </StrictMode>
+);
