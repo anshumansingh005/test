9e684e3f6ee3280bcfd6f7e76e76afc4bb23e019
Anshuman Singh <anshumansingh005@gmail.com>
2024-08-25 03:10:15 +0530

    removed container.

--- crud-react/src/App.jsx
+++ crud-react/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
 import "./App.css";
 import Create from "./Components/Create";
 function App() {
   return (
-    <Container>
+    <>
       <h1>CRUD using React</h1>
       <Create />
-    </Container>
+    </>
   );
 }
 
